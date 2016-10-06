e16d5a0d42e2817f8e1ac9361a891e17f5f54f2b
Naledi Madlopha <naledi.madlopha@gmail.com>
2016-10-06 21:30:16 +0200

    refactor: remove the api request file

--- app/src/main/java/com/popular_movies/app/GlobalConstant.java
+++ app/src/main/java/com/popular_movies/app/GlobalConstant.java
@@ -7,27 +7,13 @@ package com.popular_movies.app;
  * Provides static values to the entire application
  */
 public class GlobalConstant {
-    public final static String RESULTS = "results";
-    public final static String ID = "id";
-    public final static String POSTER_PATH = "poster_path";
-    public final static String ORIGINAL_TITLE = "original_title";
-    public final static String TITLE = "title";
-    public final static String OVERVIEW = "overview";
-    public final static String GENRE_IDS = "genre_ids";
+
     public final static String GENRES = "genres";
-    public final static String RELEASE_DATE = "release_date";
-    public final static String BACKDROP_PATH = "backdrop_path";
-    public final static String POPULARITY = "popularity";
-    public final static String VOTE_COUNT = "vote_count";
-    public final static String VOTE_AVERAGE = "vote_average";
+
     // Replace the value of this variable with an API KEY
     public final static String C5CA40DED62975B80638B7357FD69E9 = "9c5ca40ded62975b80638b7357fd69e9";
     public final static String HTTPS_API_THEMOVIEDB_ORG_3 = "https://api.themoviedb.org/3";
-    public final static String API_KEY_PARAMETER = "?api_key=" + C5CA40DED62975B80638B7357FD69E9;
     public final static String API_KEY = "api_key";
-
-    public final static String BASE_QUERY_URL = HTTPS_API_THEMOVIEDB_ORG_3 + "/movie/";
-    public final static String SINGLE_MOVIE_QUERY = "/movie/";
     public final static String MOVIE_POPULAR = "/movie/popular";
     public final static String MOVIE_TOP_RATED = "/movie/top_rated";
 
@@ -40,22 +26,9 @@ public class GlobalConstant {
 
     public final static String QUERY_GENRE = HTTPS_API_THEMOVIEDB_ORG_3
             + GlobalConstant.GENRE_MOVIE_LIST;
-
-
-
-
     public final static String GENRE_MOVIE_LIST = "/genre/movie/list";
     public final static String MOVIE = "movie";
     public final static String MOVIES = "movies";
-    public final static String NAME = "name";
-    public final static String ISO_639_1 = "iso_639_1";
-    public final static String ISO_3166_1 = "iso_3166_1";
-    public final static String KEY = "key";
-    public final static String SITE = "site";
-    public final static String SIZE = "size";
-    public final static String TYPE = "type";
-    public final static String AUTHOR = "author";
-    public final static String CONTENT = "content";
     public final static String VND_YOUTUBE = "vnd.youtube:";
     public final static String HTTP_WWW_YOUTUBE_COM_WATCH_V = "http://www.youtube.com/watch?v=";
     public final static String FAVOURITE_MOVIE_LIST = "FavouriteMovieList";
@@ -65,11 +38,5 @@ public class GlobalConstant {
     public final static String TOP_RATED = "Top Rated";
     public final static String FAVOURITE = "Favourite";
 
-    // The authority for the sync adapter's content provider
-    public final static String AUTHORITY = "com.popular_movies.datasync.provider";
-    // An account type, in the form of a domain name
-    public final static String ACCOUNT_TYPE = "themoviedb.org";
-    // The account name
-    public final static String ACCOUNT = "default_account";
 
 }
